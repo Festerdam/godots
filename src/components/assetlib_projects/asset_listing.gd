a774abd8ac7449e0517cbdfd832cc7663f042ca7
Festerdam <festerdam@posteo.net>
2023-08-31 18:03:39 +0100

    Implement get_assetlib_url

--- src/components/assetlib_projects/asset_listing.gd
+++ src/components/assetlib_projects/asset_listing.gd
@@ -1,8 +1,9 @@
 extends HBoxContainer
 
 
-var id: int
+const _ASSET_URL_PREFIX = "https://godotengine.org/asset-library/api/asset/"
 
+var _id: int
 var _title: String
 var _category: String
 var _author: String
@@ -23,9 +24,14 @@ func _ready():
 
 func init(id: int, title: String, category: String, author: String,
 		license: String):
-	self.id = id
+	_id = id
 	_title = title
 	_category = category
 	_author = author
 	_license = license
 	return self
+
+
+## Returns the asset's url in the asset library.
+func get_assetlib_url() -> String:
+	return _ASSET_URL_PREFIX.path_join(str(_id))
